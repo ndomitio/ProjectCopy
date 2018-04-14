9d0f80e15c946e1bff8febc2e1f11e7d6f85a544
NicholasDomitio <nicholas.domitio@gmail.com>
2018-04-14 19:02:24 -0400

    first

---
+++ PhytRxProject/Controllers/LogsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PhytRxProject.Models;
+
+namespace PhytRxProject.Controllers
+{
+    public class LogsController : Controller
+    {
+        private Entities1 db = new Entities1();
+
+        // GET: Logs
+        public ActionResult Index()
+        {
+            var logs = db.Logs.Include(l => l.Exercis).Include(l => l.Rx2);
+            return View(logs.ToList());
+        }
+
+        // GET: Logs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Log log = db.Logs.Find(id);
+            if (log == null)
+            {
+                return HttpNotFound();
+            }
+            return View(log);
+        }
+
+        // GET: Logs/Create
+        public ActionResult Create()
+        {
+            ViewBag.ExID = new SelectList(db.Exercises, "ExID", "ExName");
+            ViewBag.RXID = new SelectList(db.Rx2, "RXID", "RxName");
+            return View();
+        }
+
+        // POST: Logs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "LogID,PainNumb,DiffNumb,ComTxt,IsComplete,FullSet,PartialSet,PID,PhID,RXID,ExID")] Log log)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Logs.Add(log);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ExID = new SelectList(db.Exercises, "ExID", "ExName", log.ExID);
+            ViewBag.RXID = new SelectList(db.Rx2, "RXID", "RxName", log.RXID);
+            return View(log);
+        }
+
+        // GET: Logs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Log log = db.Logs.Find(id);
+            if (log == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ExID = new SelectList(db.Exercises, "ExID", "ExName", log.ExID);
+            ViewBag.RXID = new SelectList(db.Rx2, "RXID", "RxName", log.RXID);
+            return View(log);
+        }
+
+        // POST: Logs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "LogID,PainNumb,DiffNumb,ComTxt,IsComplete,FullSet,PartialSet,PID,PhID,RXID,ExID")] Log log)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(log).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ExID = new SelectList(db.Exercises, "ExID", "ExName", log.ExID);
+            ViewBag.RXID = new SelectList(db.Rx2, "RXID", "RxName", log.RXID);
+            return View(log);
+        }
+
+        // GET: Logs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Log log = db.Logs.Find(id);
+            if (log == null)
+            {
+                return HttpNotFound();
+            }
+            return View(log);
+        }
+
+        // POST: Logs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Log log = db.Logs.Find(id);
+            db.Logs.Remove(log);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PhytRxProject/Controllers/Rx2Controller.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PhytRxProject.Models;
+
+namespace PhytRxProject.Controllers
+{
+    public class Rx2Controller : Controller
+    {
+        private Entities1 db = new Entities1();
+
+        // GET: Rx2
+        public ActionResult Index(int pID)
+        {
+            var rx2 = db.Rx2.Include(r => r.Patient);
+            ViewBag.PID = pID;
+            return View(rx2.ToList());
+        }
+        
+        public ActionResult ViewPatient()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult MyAction(string search)
+        {
+
+            //do whatever you need with the parameter, 
+            //like using it as parameter in Linq to Entities or Linq to Sql, etc. 
+            //Suppose your search result will be put in variable "result".
+            ViewData.Model = db.Rx2;
+            return View();
+
+            
+
+
+        }
+        // GET: Rx2/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rx2 rx2 = db.Rx2.Find(id);
+            if (rx2 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rx2);
+        }
+
+        // GET: Rx2/Create
+        public ActionResult Create(int pID)
+        {
+            ViewBag.PID = pID;
+            //ViewBag.PID = new SelectList(db.Patients, "PID", "UserID");
+            return View();
+        }
+
+        // POST: Rx2/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RXID,RxName,ExID,PID,PhID,LogID,UsersName")] Rx2 rx2)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Rx2.Add(rx2);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.PID = new SelectList(db.Patients, "PID", "UserID", rx2.PID);
+            return View(rx2);
+        }
+
+        // GET: Rx2/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rx2 rx2 = db.Rx2.Find(id);
+            if (rx2 == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.PID = new SelectList(db.Patients, "PID", "UserID", rx2.PID);
+            return View(rx2);
+        }
+
+        // POST: Rx2/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RXID,RxName,ExID,PID,PhID,LogID,UsersName")] Rx2 rx2)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(rx2).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.PID = new SelectList(db.Patients, "PID", "UserID", rx2.PID);
+            return View(rx2);
+        }
+
+        // GET: Rx2/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rx2 rx2 = db.Rx2.Find(id);
+            if (rx2 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rx2);
+        }
+
+        // POST: Rx2/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Rx2 rx2 = db.Rx2.Find(id);
+            db.Rx2.Remove(rx2);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
